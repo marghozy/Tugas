6b0b1edc8d6755eb9d150f241f226e873324e5c6
marghozy <162287890+marghozy@users.noreply.github.com>
2024-10-24 15:19:44 +0700

    Update Main3.java

--- ETS/Main3.java
+++ ETS/Main3.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Membuat objek ParkingTicketMachine dengan harga tiket Rp5.000 per jam
+        // Membuat mesin tiket parkir dengan harga Rp5.000 per jam
         ParkingTicketMachine machine = new ParkingTicketMachine(5000);
 
         // Menambahkan uang sebesar Rp10.000
         machine.insertMoney(10000);
 
-        // Mengeluarkan tiket
-        machine.issueTicket(); // Tiket dikeluarkan untuk 2 jam parkir.
+        // Mengeluarkan tiket parkir
+        machine.issueTicket(); // Tiket dikeluarkan untuk 2 jam parkir
 
         // Menampilkan waktu parkir yang dibeli
         System.out.println("Waktu parkir yang dibeli: " + machine.getTimePurchased() + " jam.");
