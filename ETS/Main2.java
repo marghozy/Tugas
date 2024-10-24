961df9ed9750da7c49d521c0300b16932f19a404
marghozy <162287890+marghozy@users.noreply.github.com>
2024-10-24 14:51:53 +0700

    Create Main2.java

---
+++ ETS/Main2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Membuat objek Book
+        Book buku = new Book("Jago Mobile Legends", "Rafif Farhan Zaky", 2024, "Raja Jawa", "Ilmu Mahal");
+
+        // Memanggil metode printDetails untuk mencetak informasi buku
+        buku.printDetails();
+    }
+}
