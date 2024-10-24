f81118b4575ce5b36e40888aff582c47e426010c
marghozy <162287890+marghozy@users.noreply.github.com>
2024-10-24 16:03:53 +0700

    Update Main1.java

--- ETS/Main1.java
+++ ETS/Main1.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Kucing kucingku = new Kucing("Boo", 3, "Anggora");
-        kucingku.tidur(); // Metode dari kelas induk
-        kucingku.makan(); // Metode dari kelas turunan
+        kucingku.tidur();
+        kucingku.makan();
     }
 }
