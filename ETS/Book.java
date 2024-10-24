a335531d3bf3670707576200a63035389f3f60f3
marghozy <162287890+marghozy@users.noreply.github.com>
2024-10-24 14:46:09 +0700

    Update Book.java

--- ETS/Book.java
+++ ETS/Book.java
@@ -1,1 +1,26 @@
+class Book {
+    // Atribut kelas
+    String judul;
+    String penulis;
+    int tahunTerbit;
+    String namaPenerbit;
+    String kategoriBuku;
 
+    // Konstruktor untuk menginisialisasi atribut
+    public Book(String judul, String penulis, int tahunTerbit, String namaPenerbit, String kategoriBuku) {
+        this.judul = judul;
+        this.penulis = penulis;
+        this.tahunTerbit = tahunTerbit;
+        this.namaPenerbit = namaPenerbit;
+        this.kategoriBuku = kategoriBuku;
+    }
+
+    // Metode untuk mencetak detail buku
+    public void printDetails() {
+        System.out.println("Judul Buku: " + judul);
+        System.out.println("Penulis: " + penulis);
+        System.out.println("Tahun Terbit: " + tahunTerbit);
+        System.out.println("Penerbit: " + namaPenerbit);
+        System.out.println("Kategori: " + kategoriBuku);
+    }
+}
