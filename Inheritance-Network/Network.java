fb5a2ee91a0573da88974d2964b41234b3a7ba42
marghozy <162287890+marghozy@users.noreply.github.com>
2024-11-12 21:30:59 +0700

    Update Network.java

--- Inheritance-Network/Network.java
+++ Inheritance-Network/Network.java
@@ -1,36 +1,30 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Network {
-    private ArrayList<User> users;
-    private ArrayList<Post> posts;
+    private List<User> users;
+    private List<Post> posts;
 
     public Network() {
         users = new ArrayList<>();
         posts = new ArrayList<>();
     }
 
-    public void addUser(String username, String password) {
-        User newUser = new User(username, password);
-        users.add(newUser);
+    public User addUser(String username) {
+        User user = new User(username);
+        users.add(user);
+        System.out.println("User added: " + username);
+        return user;
     }
 
-    public void addPost(String content, User author) {
-        Post newPost = new Post(content, author);
-        posts.add(newPost);
+    public void addPost(Post post) {
+        posts.add(post);
     }
 
-    public User findUser(String username) {
-        for (User user : users) {
-            if (user.getUsername().equals(username)) {
-                return user;
-            }
-        }
-        return null;
-    }
-
-    public void displayPosts() {
+    public void showAllPosts() {
+        System.out.println("\nAll Posts:");
         for (Post post : posts) {
-            System.out.println(post);
+            System.out.println(post.getAuthor().getUsername() + " posted: " + post.getContent() + " - Likes: " + post.getLikeCount());
         }
     }
 }
