a6749b81fe0544a3d080a7f72d6d499a6b770dba
marghozy <162287890+marghozy@users.noreply.github.com>
2024-11-28 19:56:43 +0700

    Create Foxes.java

---
+++ Rabbit Foxes/Foxes.java
@@ -0,0 +1,39 @@
+import java.util.List;
+
+public class Foxes extends Animal {
+
+    public Foxes(Field field, Location location) {
+        super(field, location);
+    }
+
+    @Override
+    public void act(List<Animal> newAnimals) {
+        if (isAlive()) {
+            Location newLocation = findFood();
+            if (newLocation == null) {
+                newLocation = getField().freeAdjacentLocation(getLocation());
+            }
+
+            if (newLocation != null) {
+                setLocation(newLocation);
+            } else {
+                setDead();
+            }
+        }
+    }
+
+    private Location findFood() {
+        List<Location> adjacent = getField().adjacentLocations(getLocation());
+
+        for (Location loc : adjacent) {
+            Object animal = getField().getObjectAt(loc);
+            if (animal instanceof Rabbit) {
+                Rabbit rabbit = (Rabbit) animal;
+                rabbit.setDead();
+                return loc;
+            }
+        }
+
+        return null;
+    }
+}
